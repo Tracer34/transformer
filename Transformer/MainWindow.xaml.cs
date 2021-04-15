ed7b3f00a52117969d0a2aed3f88be370d41b173
Denis Izvarin <dizvarin@mail.ru>
2021-04-15 23:18:57 +0300

    Initial commit

---
+++ Transformer/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+
+namespace Transformer
+{
+    public partial class MainWindow : Window
+    {
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable transformerTable;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // получаем строку подключения из app.config
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            transformerGrid.RowEditEnding += TransformerGrid_RowEditEnding;
+        }
+
+        private void TransformerGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            UpdateDB();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM Transformer";
+            transformerTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
+                // установка команды на добавление для вызова хранимой процедуры
+                adapter.InsertCommand = new SqlCommand("sp_InsertTransformer", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@title", SqlDbType.NVarChar, 50, "Title"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@company", SqlDbType.NVarChar, 50, "Company"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@price", SqlDbType.Int, 0, "Price"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@power", SqlDbType.NVarChar, 0, "Power"));
+                SqlParameter parameter = adapter.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
+                parameter.Direction = ParameterDirection.Output;
+
+                connection.Open();
+                adapter.Fill(transformerTable);
+                transformerGrid.ItemsSource = transformerTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        private void UpdateDB()
+        {
+            SqlCommandBuilder comandbuilder = new SqlCommandBuilder(adapter);
+            adapter.Update(transformerTable);
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateDB();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (transformerGrid.SelectedItems != null)
+            {
+                for (int i = 0; i < transformerGrid.SelectedItems.Count; i++)
+                {
+                    DataRowView datarowView = transformerGrid.SelectedItems[i] as DataRowView;
+                    if (datarowView != null)
+                    {
+                        DataRow dataRow = (DataRow)datarowView.Row;
+                        dataRow.Delete();
+                    }
+                }
+            }
+            UpdateDB();
+        }
+    }
+}
